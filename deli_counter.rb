cadeb0945b5b7a71dd45c78bea37d13881f2c1c9
Matt Hough <matt@hough.tv>
2017-09-25 22:30:32 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,4 +20,11 @@ def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
 end
 
-line(katz_deli)
+def now_serving(katz_deli)
+  if katz_deli.size != 0
+    puts "#{katz_deli[0]}"
+    katz_deli.shift
+  else
+    "There is nobody waiting to be served!"
+end
+
