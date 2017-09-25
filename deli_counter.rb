29f75f02fce8db44a4eddf1ee46a602284be3974
Matt Hough <matt@hough.tv>
2017-09-25 22:22:15 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+require "pry"
+
+katz_deli = ["Gena", "Bob", "Geoffrey", "Jefferson", "Thomas"]
+
+def line(katz_deli)
+  if katz_deli.size != 0
+    the_line = "The line is currently:"
+    katz_deli.each.with_index do |pos, index|
+      the_line << " #{index + 1}. #{pos}"
+    end
+    puts the_line
+  else
+    puts "The line is currently empty."
+  end
+  #binding.pry
+end
+
+def take_a_number(katz_deli, name)
+  "#{name}"
+  "#{katz_deli.size}"
+end
+
+line(katz_deli)
