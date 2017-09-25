e29fc2d4075332b8ef628e46edc6a8aade91b471
Matt Hough <matt@hough.tv>
2017-09-25 22:26:58 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,8 +16,8 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli, name)
-    katz_deli << "#{name}"
-    puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
+  katz_deli << "#{name}"
+  puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
 end
 
 line(katz_deli)
